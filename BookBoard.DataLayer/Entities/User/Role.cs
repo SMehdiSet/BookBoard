a723899d43d873222fb27ad215eae34a721a6c25
Seyed Mehdi Setodegun <seyedmehdisetodegun@gmail.com>
2024-04-08 19:59:11 +0330

    Add User Folder => Role Class

---
+++ BookBoard.DataLayer/Entities/User/Role.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookBoard.DataLayer.Entities.User
+{
+    public class Role
+    {
+        [Key]
+        public int RoleId { get; set; }
+
+        [Display(Name = "")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید.")]
+        [MaxLength(200, ErrorMessage = "{0} نمیتواند بیش از {1} کاراکتر داشته باشد.")]
+        public string RoleTitle { get; set; }
+    }
+}
