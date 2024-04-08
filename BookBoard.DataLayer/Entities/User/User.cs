ce8144288a140b017ba5b2c5ed68a61cac1d5705
Seyed Mehdi Setodegun <seyedmehdisetodegun@gmail.com>
2024-04-08 20:09:45 +0330

    Add User Folder => User Class

---
+++ BookBoard.DataLayer/Entities/User/User.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookBoard.DataLayer.Entities.User
+{
+    public class User
+    {
+        [Key]
+        public int UserId { get; set; }
+
+        [Display(Name = "نام کاربری")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید.")]
+        [MaxLength(200, ErrorMessage = "{0} نمیتواند بیش از {1} کاراکتر داشته باشد.")]
+        public string UserName { get; set; }
+
+        [Display(Name = "ایمیل")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید.")]
+        [MaxLength(200, ErrorMessage = "{0} نمیتواند بیش از {1} کاراکتر داشته باشد.")]
+        [EmailAddress(ErrorMessage = "ایمیل وارد شده معتبر نمیباشد.")]
+        public string Email { get; set; }
+
+        [Display(Name = "کلمه عبور")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید.")]
+        [MaxLength(200, ErrorMessage = "{0} نمیتواند بیش از {1} کاراکتر داشته باشد.")]
+        public string Password { get; set; }
+
+
+        [Display(Name = "کد فعالسازی")]
+        public string ActiveCode { get; set; }
+
+        [Display(Name = "وضعیت")]
+        public bool IsActive { get; set; }
+        
+        [Display(Name = "آواتار")]
+        public string UserAvatar { get; set; }
+        
+        [Display(Name = "تاریخ ثبت نام")]
+        public DateTime RegisterDate { get; set; }
+    }
+}
