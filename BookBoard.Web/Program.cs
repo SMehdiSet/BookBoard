3b81711cff3b28d9a9e3fd537b609e0e69cdd524
Seyed Mehdi Setodegun <seyedmehdisetodegun@gmail.com>
2024-04-09 12:47:22 +0330

    Create DataBase

--- BookBoard.Web/Program.cs
+++ BookBoard.Web/Program.cs
@@ -1,4 +1,16 @@
+using BookBoard.DataLayer.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+#region DateBase Context
+
+builder.Services.AddDbContext<BookBoardContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("BookBoardConnection"));
+});
+
+#endregion
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
