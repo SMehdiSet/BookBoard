58b95077fa7142c3feddb5efa01b38daccb9e49f
Seyed Mehdi Setodegun <seyedmehdisetodegun@gmail.com>
2024-04-09 12:17:08 +0330

    Add User Folder => UserRole Class

---
+++ BookBoard.DataLayer/Entities/User/UserRole.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookBoard.DataLayer.Entities.User
+{
+    public class UserRole
+    {
+        public UserRole()
+        {
+            
+        }
+
+        [Key]
+        public int URId { get; set; }
+        public int UserId { get; set; }
+        public int RoleId { get; set; }
+    }
+}
