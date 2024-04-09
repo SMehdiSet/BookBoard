3af335c28a75fe39de2f2dede3ded9d640692b5f
Seyed Mehdi Setodegun <seyedmehdisetodegun@gmail.com>
2024-04-09 12:25:10 +0330

    Add BookBoardContext

---
+++ BookBoard.DataLayer/Context/BookBoardContext.cs
@@ -0,0 +1,27 @@
+﻿using BookBoard.DataLayer.Entities.User;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookBoard.DataLayer.Context
+{
+    public class BookBoardContext : DbContext
+    {
+        public BookBoardContext(DbContextOptions<BookBoardContext> options) : base(options)
+        {
+
+        }
+
+        #region User
+
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<UserRole> UserRoles { get; set; }
+
+        #endregion
+
+    }
+}
